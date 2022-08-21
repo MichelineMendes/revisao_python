9989732f22d32efbe77f4e5b89ce10b60ff9f956
MichelineMendes <micheline_mendes@hotmail.com>
2022-08-21 14:49:26 -0300

    commit inicial

--- q6_idade_alt.py
+++ q6_idade_alt.py
@@ -1,6 +1,6 @@
 # Faça um Programa que peça a idade e a altura de 5 pessoas,
 # armazene cada informação no seu respectivo vetor.
-# Imprima a idade e a altura na ordem inversa a ordem lida.
+# Imprima a idade e a altura na ordem inversa a ordem lida
 print("*******  Inverter ondem de números  ******")
 idades = []
 alturas = []
