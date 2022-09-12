ee9c8435b361fece72b659b5ec91812840f0fa4a
MichelineMendes <103367621+MichelineMendes@users.noreply.github.com>
2022-09-12 17:50:23 -0300

    Update q5_primo.py

--- q5_primo.py
+++ q5_primo.py
@@ -5,3 +5,15 @@ numero = int(input("digite um numero para saber todos os primos existentes menor
 for x in range(2, numero+1):
     if x ==2 or x%2 !=0:
        print(x, end= " , ")
+    
+    
+n = int(input("Digite um número para saber os seus divisores primos: "))
+lista= []
+for i in range(1, n + 1):
+    if i > 1:
+        for j in range(2, i):
+            if (i % j == 0):
+                break
+        else:
+            lista.append(i)
+print(lista)
