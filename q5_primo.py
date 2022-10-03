fada8bf4ba01acf4bfb090a3d6e3bab07fb0321a
MichelineMendes <103367621+MichelineMendes@users.noreply.github.com>
2022-10-03 20:08:56 -0300

    Update q5_primo.py

--- q5_primo.py
+++ q5_primo.py
@@ -2,10 +2,7 @@
 # entre 1 e um número inteiro informado pelo usuário.
 print("*******  Verificar primos existentes  ******")
 numero = int(input("digite um numero para saber todos os primos existentes menores que esse número "))
-for x in range(2, numero+1):
-    if x ==2 or x%2 !=0:
-       print(x, end= " , ")
-    
+  
     
 n = int(input("Digite um número para saber os seus divisores primos: "))
 lista= []
